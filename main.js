813ea6663574aff35eb2100758d8461f11f657ea
Denys <d.o.sheremet@student.tudelft.nl>
2021-12-09 20:01:02 +0100

    dropdown of buttons and toggles possible answer buttons

--- main.js
+++ main.js
@@ -2,16 +2,31 @@ const deletions = ["Simple Deletion", "Comparative Deletion", "Lack of Referenti
 const distortions = ["Nominalization", "Cause and Effect", "Complex Equivalence", "Mind Reading", "Lost Comparative"];
 const generalizations = ["Universal Quantifiers", "Modal Operator of Necessity", "Modal Operator of Possibility", "Presupposition"];
 const mappings = deletions.concat(distortions, generalizations);
-const mappingTypes = [deletions, distortions, generalizations];
+
 var currentSentence = "";
 var currentMapping = "";
+
+var dropdownShowing = false;
+var activeMappings = [...mappings];
+
+
+function showDropdown() {
+    if (dropdownShowing) {
+        document.getElementById("myDropdown").style.display = "none";
+    } else {
+        document.getElementById("myDropdown").style.display = "block";
+    }
+    dropdownShowing = !dropdownShowing;
+
+}
+
 
 
 function answerChosen(answer) {
     if (answer === currentMapping) {
         alert("Correct Answer!");
     } else {
-        alert("Wrong Answer! \nCorrect answer: " + currentMapping );
+        alert("Wrong Answer! \nCorrect answer: " + currentMapping);
     };
     chooseSentence();
 };
@@ -22,28 +37,60 @@ function chooseSentence() {
     document.getElementById("sentence").innerHTML = currentSentence;
 };
 
+function selectMappings(selectedMapping) {
+    console.log(selectedMapping);
+
+    var index = activeMappings.indexOf(selectedMapping);
+    if (index != -1) {
+        document.getElementById("dropdown:" + selectedMapping).style.backgroundColor = "gray";
+        document.getElementById("answer:" + selectedMapping).style.display = "none";
+        activeMappings.splice(index, 1);
+
+    } else {
+        document.getElementById("dropdown:" + selectedMapping).style.backgroundColor = "cyan";
+        document.getElementById("answer:" + selectedMapping).style.display = "block";
+        activeMappings.push(selectedMapping);
+    };
+    console.log(activeMappings);
+}
+
+
 window.addEventListener("load", function () {
+
+    //Add dropdown buttons
+    for (var i = 0; i < mappings.length; i++) {
+        let btn = document.createElement("button");
+        btn.id = "dropdown:" + mappings[i];
+        btn.className = "dropdownBtn";
+        console.log(btn.id);
+        btn.innerHTML = mappings[i];
+        btn.onclick = function () { selectMappings(btn.innerHTML); };
+        document.getElementById("myDropdown").appendChild(btn);
+    }
 
     // generate Buttons for different answers
     for (let i = 0; i < deletions.length; i++) {
         let btn = document.createElement("button");
+        btn.id = "answer:" + deletions[i];
         btn.innerHTML = deletions[i];
         btn.className = "mappingButton";
-        btn.onclick = function() { answerChosen(deletions[i]); };
+        btn.onclick = function () { answerChosen(deletions[i]); };
         document.getElementById("deletionButtons").appendChild(btn);
     }
     for (let i = 0; i < distortions.length; i++) {
         let btn = document.createElement("button");
+        btn.id = "answer:" + distortions[i];
         btn.innerHTML = distortions[i];
         btn.className = "mappingButton";
-        btn.onclick = function() { answerChosen(distortions[i]); };
+        btn.onclick = function () { answerChosen(distortions[i]); };
         document.getElementById("distortionButtons").appendChild(btn);
     }
     for (let i = 0; i < generalizations.length; i++) {
         let btn = document.createElement("button");
+        btn.id = "answer:" + generalizations[i];
         btn.innerHTML = generalizations[i];
         btn.className = "mappingButton";
-        btn.onclick = function() { answerChosen(generalizations[i]); };
+        btn.onclick = function () { answerChosen(generalizations[i]); };
         document.getElementById("generalizationButtons").appendChild(btn);
     }
     chooseSentence();
